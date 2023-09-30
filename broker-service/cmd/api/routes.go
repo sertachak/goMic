03415fbf59605401e6d4444117d85e73594ea1b0
sertachak <sertaconurhakbilen@gmail.com>
2023-09-30 20:57:38 +0300

    Initial commit

---
+++ broker-service/cmd/api/routes.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
+	"net/http"
+)
+import (
+	"github.com/go-chi/chi"
+)
+
+func (app *Config) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://*", "https://*"},
+		AllowedMethods:   []string{"GET", "PUT", "POST", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	mux.Use(middleware.Heartbeat("/ping"))
+
+	return mux
+}
